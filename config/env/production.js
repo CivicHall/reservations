06196ff355f5c14effe59fb1435af44965d3edc1
slifty <slifty@gmail.com>
2015-09-03 23:16:53 -0400

    added production config file

--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  app: {
+    title: 'Cobot Reservations',
+    description: 'Visualize the room reservations of a Cobot instance',
+    keywords: 'cobot, visualization, reservations'
+  },
+  port: 80,
+  cobot: {
+    host: process.env['COBOT_HOST'],
+    client_id: process.env['COBOT_CLIENT_ID'],
+    client_secret: process.env['COBOT_CLIENT_SECRET'],
+    username: process.env['COBOT_USERNAME'],
+    password: process.env['COBOT_PASSWORD']
+  }
+}
