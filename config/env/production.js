083640b264efba5c24963a00b4cae98c8991627f
slifty <slifty@gmail.com>
2015-09-03 23:29:59 -0400

    port is properly linked to env variables

--- config/env/production.js
+++ config/env/production.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Visualize the room reservations of a Cobot instance',
     keywords: 'cobot, visualization, reservations'
   },
-  port: 3000,
+  port: process.env.['PORT'] || 8080,
   cobot: {
     host: process.env['COBOT_HOST'],
     client_id: process.env['COBOT_CLIENT_ID'],
